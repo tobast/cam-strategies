cabe3e45296297b8866eb97a4c557cb9108d4a44
Théophile Bastian <contact@tobast.fr>
2016-06-14 14:09:29 +0100

    First try for datatypes

---
+++ datatypes.ml
@@ -0,0 +1,70 @@
+
+(*
+ *  Strategies interpreter
+ * 
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+(** Defines the data types that will be used to represent strategies. *)
+
+(** {6 DAG datatypes } *)
+
+(** Unique node identifier session-wide (ie. two nodes, even from different
+ DAGs, will not have the same nodeId). *)
+type nodeId = int
+
+(** Node in a DAG. The name is only used for display purposes. *)
+type dagNode = {
+    nodeId : nodeId ;
+    nodeName : string ;
+    mutable nodeOutEdges : dagEdge list ;
+    mutable nodeInEdges : dagEdge list
+}
+
+and dagEdge = {
+    edgeSrc : dagNode ;
+    edgeDst : dagNode
+}
+
+module DagNode = struct
+    type t = dagNode
+    let compare a b =
+        Pervasives.compare a.nodeId b.nodeId
+end
+
+module NodeSet = Set.Make(DagNode)
+module NodeMap = Map.Make(DagNode)
+
+type dag = NodeSet.t
+
+(** {6 Games, strategies} *)
+
+(** Polarity of an event. *)
+type polarity = PolNeg | PolNeutral | PolPos
+
+type esp = {
+    evts : dag ;
+    pol : polarity NodeMap.t
+}
+
+type game = esp
+
+type strategy = {
+    strat : esp ;
+    game : game ;
+    
+    (** Maps a node of the strategy to a node of the game *)
+    map : dagNode NodeMap.t
+}
+
