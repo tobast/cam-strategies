ec1742ab1d0a1c9319d3d96c2da407dfa118dd75
Théophile Bastian <contact@tobast.fr>
2016-06-16 17:16:07 +0100

    First step to a working pullback — by now, elements with dependancies seem to be ignored

--- operations/pullback.ml
+++ operations/pullback.ml
@@ -23,8 +23,108 @@ module type S = sig
 end
 
 module BottomUp = struct
+    let invertedMapping strategy =
+        let rev =
+            NodeMap.fold (fun sNode gNode cur ->
+                NodeMap.add gNode sNode cur) strategy.st_map
+                NodeMap.empty in
+        (*
+        NodeSet.fold (fun evt cur ->
+            if NodeMap.mem evt cur then cur
+                else NodeMap.add evt None cur) strategy.st_game.evts rev
+        *) rev
+
     let pullback s1 s2 =
-        assert false
+        (* NOTE for now, everything is deterministic. Thus, each stragety
+         * contains at most ONE copy of each event of the game.
+         * This represetation WON'T WORK anymore when we'll plug
+         * non-determinism in it.
+         *)
+        (* Copy: makes it possible to reuse the nodes directly on the pullback
+         * and to use them on a map all together. *)
+        
+        if s1.st_game <> s2.st_game then
+            raise MismatchedGames;
+        let game = s1.st_game in
+
+        let cs1 = Builder.strat_copy s1
+        and cs2 = (* Builder.strat_copy *) s2 in
+        let eventsSet = NodeSet.union cs1.st_strat.evts cs2.st_strat.evts in
+        
+        let invmap1 = invertedMapping cs1
+        and invmap2 = invertedMapping cs2 in
+        
+        (* Sets of dependancy: which events the given event depends on? *)
+        let depSets = NodeSet.fold (fun evt cur ->
+                NodeMap.add evt (List.fold_left (fun cSet cEdge ->
+                    NodeSet.add cEdge.edgeSrc cSet)
+                    NodeSet.empty evt.nodeInEdges) cur)
+            eventsSet NodeMap.empty in
+        
+        let hasDeps depsSet invmap evt =
+            (try
+                let sEvt = NodeMap.find evt invmap in
+                not @@ NodeSet.is_empty @@ NodeMap.find sEvt depSets
+            with Not_found -> true)
+        in
+        let hasAnyDeps depSets gEvt =
+            hasDeps depSets invmap1 gEvt || hasDeps depSets invmap2 gEvt
+        in
+        
+        let worklist = NodeSet.fold (fun gEvt cur ->
+            if not @@ hasAnyDeps depSets gEvt
+                then gEvt :: cur
+                else cur) game.evts [] in
+        
+        let rec remDeps strat (deps,newFree) nd = function
+        | [] -> deps,newFree
+        | hd::tl ->
+            let cDeps = NodeMap.find hd deps in
+            let nDeps = NodeSet.remove nd cDeps in
+            let nFree =
+                if NodeSet.is_empty nDeps && (not @@ NodeSet.is_empty cDeps)
+                    then NodeSet.add (Helpers.getGameNode hd strat) newFree
+                    else newFree in
+            
+            remDeps strat ((NodeMap.add hd nDeps deps),nFree) nd tl
+        in
+
+        let rec buildPullback pbNodesMap depSets cPb = function
+        | [] -> cPb
+        | cEvt::tl ->
+            let sEv1 = NodeMap.find cEvt invmap1 in
+            let sEv2 = NodeMap.find cEvt invmap2 in
+            let name = if sEv1.nodeName = sEv2.nodeName
+                then sEv1.nodeName
+                else sEv1.nodeName ^","^sEv2.nodeName in
+            let nNode, nPb = Builder.strat_addNamedEvent name cEvt cPb in 
+            
+            let evtDeps = NodeSet.fold (fun evt cur ->
+                    NodeSet.add (NodeMap.find evt pbNodesMap) cur)
+                (NodeSet.union
+                    (NodeMap.find sEv1 depSets) (NodeMap.find sEv2 depSets))
+                NodeSet.empty
+            in
+            
+            NodeSet.iter (fun evt -> Builder.strat_addEdge evt nNode) evtDeps;
+            
+            let nDepSets,nFree = remDeps cs2
+                (remDeps cs1 (depSets,NodeSet.empty) sEv1
+                    (List.map (fun x -> x.edgeDst) sEv1.nodeOutEdges))
+                sEv2 (List.map (fun x -> x.edgeDst) sEv2.nodeOutEdges) in
+            
+            let nPbMap = NodeMap.add sEv1 nNode
+                (NodeMap.add sEv2 nNode pbNodesMap) in
+            
+            let nWorklist = NodeSet.fold (fun elt curWL ->
+                    if not @@ hasAnyDeps nDepSets elt
+                        then elt :: curWL
+                        else curWL) nFree tl in
+
+            buildPullback nPbMap nDepSets nPb nWorklist
+        in
+        
+        buildPullback NodeMap.empty depSets (Builder.strat_new game) worklist
 end
 
 module Canonical = BottomUp
