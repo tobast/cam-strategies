f8691685b47b2b37413ad135e223012b31b4486a
Théophile Bastian <contact@tobast.fr>
2016-06-23 14:57:49 +0100

    Added a test for strategies from A to B and B to A, forcing us to specify the middle game.

---
+++ strategies/compose_aba/load.ml
@@ -0,0 +1,48 @@
+open LangReader
+open Datatypes
+open Operations.Canonical
+
+let basepath = "strategies/compose_aba/"
+let gmA = readGameFile (basepath ^ "A.game")
+let gmB = readGameFile (basepath ^ "B.game")
+let perpA = perp gmA
+and perpB = perp gmB
+
+let gmA2 = Builder.game_copy gmA
+
+let nodeByName map name = SMap.find name map
+
+let gmPA_B = perpA |||: gmB
+let stratPA_B, namesPA_B = Builder.strat_newFilled_mapped gmPA_B
+let () =
+    Builder.strat_addEdge (nodeByName namesPA_B "Apos2")
+        (nodeByName namesPA_B "Aneg") ;
+    Builder.strat_addEdge (nodeByName namesPA_B "Apos1")
+        (nodeByName namesPA_B "Bpos") ;
+    Builder.strat_addEdge (nodeByName namesPA_B "Bneg1")
+        (nodeByName namesPA_B "Bpos") ;
+    Builder.strat_addEdge (nodeByName namesPA_B "Bneg2")
+        (nodeByName namesPA_B "Bpos") ;
+    Builder.strat_addEdge (nodeByName namesPA_B "Bneg2")
+        (nodeByName namesPA_B "Aneg")
+        
+let gmPB_A = perpB |||: gmA
+let stratPB_A, namesPB_A = Builder.strat_newFilled_mapped gmPB_A
+let () =
+    Builder.strat_addEdge (nodeByName namesPB_A "Bpos")
+        (nodeByName namesPB_A "Apos1") ;
+    Builder.strat_addEdge (nodeByName namesPB_A "Bpos")
+        (nodeByName namesPB_A "Apos2") ;
+    Builder.strat_addEdge (nodeByName namesPB_A "Aneg")
+        (nodeByName namesPB_A "Apos1") ;
+    Builder.strat_addEdge (nodeByName namesPB_A "Aneg")
+        (nodeByName namesPB_A "Apos2") ;
+    Builder.strat_addEdge (nodeByName namesPB_A "Aneg")
+        (nodeByName namesPB_A "Bneg2")
+
+module Comp = Composition.Canonical (Pullback.Canonical) (Parallel.Canonical)
+
+let stratCompo = Comp.compHiddenOnGame stratPA_B stratPB_A gmB
+
+let () = Printer.dotDebugOfStrategy Format.std_formatter stratCompo
+
